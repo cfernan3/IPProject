520b2e267da1c48390814aa7d1100ea79680567c
Bharat Mukheja <bkmukhej@ncsu.edu>
2016-11-13 17:09:35 -0500

    Corrected Flow Creator

--- RouteManagement.py
+++ RouteManagement.py
@@ -21,9 +21,13 @@ class RouteManagement():
         flows = []
         dst = len(path)-1
         for i in range(dst):
-            local_egress_port = switch_port_mat[path[i]][path[i+1]]
-            dst_ingress_port = switch_port_mat[path[dst]][path[dst-1]]
-            forward_flow = '{"switch":'+sw_dpid[path[i]]+',"name":'+str(self.flow_counter)+', "eth_dst":'+switch_dict[sw_dpid[path[dst]]]['port'][dst_ingress_port]['mac']+', "actions":"output='+local_egress_port+'"}'
+            forward_flow_egress_port = switch_port_mat[path[i]][path[i+1]]
+            backward_flow_egress_port = switch_port_mat[path[i+1]][path[i]]
+            dst_ingress_port_mac = switch_dict[sw_dpid[path[dst]]]['port'][switch_port_mat[path[dst]][path[dst-1]]]['mac']
+            src_egress_port_mac = switch_dict[sw_dpid[path[0]]]['port'][switch_port_mat[path[0]][path[1]]]['mac']
+            forward_flow = '{"switch":'+sw_dpid[path[i]]+',"name":'+str(self.flow_counter)+', "eth_dst":'+dst_ingress_port_mac+', "actions":"output='+forward_flow_egress_port+'"}'
+            self.flow_counter += 1
+            backward_flow = '{"switch":'+sw_dpid[path[i+1]]+',"name":'+str(self.flow_counter)+', "eth_dst":'+src_egress_port_mac+', "actions":"output='+backward_flow_egress_port+'"}'
             self.flow_counter += 1
             flows.append(forward_flow)
             flows.append(backward_flow)
